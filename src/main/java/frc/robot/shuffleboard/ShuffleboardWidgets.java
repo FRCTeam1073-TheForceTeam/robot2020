25e94da30e475f8aae0a481d052dea5a1f3b0d6a
Christian <wagener_christian@yahoo.com>
2020-01-25 15:00:36 -0500

    Simplified the code
    and added the first widgets

--- src/main/java/frc/robot/shuffleboard/ShuffleboardWidgets.java
+++ src/main/java/frc/robot/shuffleboard/ShuffleboardWidgets.java
@@ -6,40 +6,50 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot.shuffleboard;
+
 import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Robot;
+
 //import frc.robot.Robot;
 /**
  * Add your docs here.
  */
-public class ShuffleboardInformation extends SubsystemBase {
-    double leftEncoderValue;
-    double rightEncoderValue;
-    double gyroAngleDegrees;
-    double robotX;
-    double robotY;
-    double robotRotation;
-    double P;
-    double I;
-    double D;
-    double drivetrainVelocity;
+public class ShuffleboardWidgets extends SubsystemBase {
+    ShuffleboardTab tab;
+    double leftEncoderValue = 0.0;
+    double rightEncoderValue = 0.0;
+    double gyroAngleDegrees = 0.0;
+    double robotX = 0.0;
+    double robotY = 0.0;
+    double robotRotation = 0.0;
+    //double P = 0.0;
+    //double I = 0.0;
+    //double D = 0.0;
+    double drivetrainVelocity = 0.0;
 
-    public ShuffleboardInformation() {
+    public ShuffleboardWidgets() {
+
+      tab = Shuffleboard.getTab("Telemetry");
 
     }
   
-    @Override
+
+
+  @Override
     public void periodic() {
-      sensorInputs();
+      ShuffleboardInformation();
+      ShuffleboardView();
     }
   
-    public void sensorInputs() {
+    private void ShuffleboardInformation() {
 
       // Drivetrain Data INCOMPLETE
-      rightEncoderValue = Robot.subsystem.getRightEncoder();
       leftEncoderValue = Robot.subsystem.getLeftEncoder();
-
+      rightEncoderValue = Robot.subsystem.getRightEncoder();
+      
       // Gyro Datas
       gyroAngleDegrees = Robot.subsystem.getAngleDegrees();
       Pose2d pose = Robot.subsystem.getRobotPose();
@@ -58,5 +68,17 @@ public class ShuffleboardInformation extends SubsystemBase {
       // Wheel of Fortune Data MISSING
 
     } 
-    
+
+    private void ShuffleboardView() {
+
+      tab.add("leftEncoder", leftEncoderValue);
+      tab.add("rightEncoder", rightEncoderValue);
+      
+      tab.add("gyroAngle", gyroAngleDegrees);
+      tab.add("x-coordinate",robotX);
+      tab.add("y-coordinate",robotY);
+      tab.add("rotation",robotRotation);
+      
+    }
+
 }
