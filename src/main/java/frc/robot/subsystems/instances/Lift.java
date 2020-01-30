58ad9645d8cb3f80cb210033ce68c3bcb21415e7
Alex <alex.h@frc1073.org>
2020-01-29 21:06:17 -0500

    Updateth I: It believes it can fly(wheel)
    Hood motors are set up, as are encoders (I think) and basically all the things.
    
    Co-Authored-By: BenWertzDaHuman <benwertz@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/instances/Lift.java
+++ src/main/java/frc/robot/subsystems/instances/Lift.java
@@ -7,7 +7,6 @@
 
 package frc.robot.subsystems.instances;
 
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
