68f3ad4559dffc3ac35835dafa0a145602c95750
Linkd-Aether <jackmcevoy15@gmail.com>
2020-02-01 13:50:58 -0500

    Hotfix PanelManipulator Location

--- src/main/java/frc/robot/subsystems/instances/PanelManipulator.java
+++ src/main/java/frc/robot/subsystems/instances/PanelManipulator.java
@@ -5,7 +5,7 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot.subsystems;
+package frc.robot.subsystems.instances;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.interfaces.PanelManipulatorInterface;
