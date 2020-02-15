a1d6fa548b1c2bbdd033b194e48394a1dc9da6d7
Celia <36650015+classygirl44@users.noreply.github.com>
2020-02-15 16:52:24 -0500

    Changed back to the normal robot

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -23,8 +23,8 @@ public final class Main {
    * <p>If you change your main robot class, change the parameter type.
    */
   public static void main(String... args) {
-    //RobotBase.startRobot(Robot::new);
+    RobotBase.startRobot(Robot::new);
    // RobotBase.startRobot(VisionTester::new);
-    RobotBase.startRobot(RobotMercury::new);
+    // RobotBase.startRobot(RobotMercury::new);
   }
 }
