86845d1c136fcb0e956e138fe1a5434c6a179277
BenWertzDaHuman <benwertz216@gmail.com>
2020-02-17 18:40:53 -0500

    CANSparkMax integration

--- src/main/java/frc/robot/commands/ShooterIndex.java
+++ src/main/java/frc/robot/commands/ShooterIndex.java
@@ -7,39 +7,58 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.OI;
 import frc.robot.Robot;
+import frc.robot.subsystems.interfaces.ShooterInterface;
 
 public class ShooterIndex extends CommandBase {
   /**
    * Creates a new ShooterIndex.
    */
-  public ShooterIndex() {
-    addRequirements((SubsystemBase) Robot.shooter);
+  ShooterInterface shooter;
+  double loops = 0;
+  public ShooterIndex(ShooterInterface shooter_) {
+    shooter = shooter_;
+    addRequirements((SubsystemBase) shooter);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    curr=System.currentTimeMillis();
+    start = curr * 1;
   }
 
+  boolean s = false;
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    Robot.shooter.setHoodVelocity(1);
+    curr = System.currentTimeMillis();
+    shooter.setHoodPower(-0.0275);
+    SmartDashboard.putBoolean("signal", s = !s);
+    SmartDashboard.putNumber("key", loops);
+    SmartDashboard.putNumber("key3", (double) (curr-start));
+    loops++;
   }
+
+  long start = System.currentTimeMillis();
+  long curr = System.currentTimeMillis();
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    Robot.shooter.resetHood();
-    Robot.shooter.disableHood();
+    shooter.resetHood();
+    shooter.disableHood();
+    SmartDashboard.putNumber("key2", 835);
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return Robot.shooter.hoodIsIndexed();
+    return (curr - start) > 200 && shooter.hoodIsIndexed();
   }
 }
