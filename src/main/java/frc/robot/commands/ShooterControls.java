0f79b97e33b52c0b8718eb172d60d29731a4d422
Alex <alex.h@frc1073.org>
2020-02-19 21:25:57 -0500

    Revert revert
    Okay?

--- src/main/java/frc/robot/commands/ShooterControls.java
+++ src/main/java/frc/robot/commands/ShooterControls.java
@@ -7,8 +7,14 @@
 
 package frc.robot.commands;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.OI;
 import frc.robot.subsystems.interfaces.ShooterInterface;
 import frc.robot.OI;
 
@@ -26,16 +32,60 @@ public class ShooterControls extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    
+
   }
+
+  double pow = 0.5;
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     // speed = 0;
     // shooter.setFlywheelSpeed(speed);
-    shooter.setFlywheelSpeed(flyVelocity);
-    shooter.setHoodVelocity(hoodVelocity);
+    double input = (0.5 * (shooter.getMinHoodAngle() + shooter.getMaxHoodAngle())
+        + 0.5 * (shooter.getMaxHoodAngle() - shooter.getMinHoodAngle()) * OI.driverController.getRawAxis(0));
+    if (OI.driverController.getAButtonPressed()) {
+      pow = 0.5 / pow;
+    }
+    //Math.abs(OI.driverController.getRawAxis(1)) * Math.PI * 3.5;
+
+    // double aput = OI.driverController.getRawAxis(1);
+    // aput = Math.signum(aput) * Math.pow(Math.abs(aput), 0.65);
+    // shooter.setHoodPower(0.05 * aput);
+
+    // if (OI.driverController.getBumper(Hand.kLeft)) {
+    //   shooter.setHoodPower(0.1*OI.driverController.getRawAxis(5));
+    // }
+    // if (OI.driverController.getBumperReleased(Hand.kLeft)) {
+    //   shooter.resetHood();
+    // }
+
+
+    // shooter.setHoodPower(0.1 * OI.driverController.getRawAxis(1));
+    
+    
+    // shooter.setHoodAngle((shooter.getMaxHoodAngle() + shooter.getMinHoodAngle()) * 0.5
+      // + (shooter.getMaxHoodAngle() - shooter.getMinHoodAngle()) * 0.5 * OI.driverController.getRawAxis(1));
+
+    shooter.setFlywheelPower(0.2 * pow * OI.driverController.getRawAxis(1));
+
+    // if (OI.driverController.getAButton()) {
+    //   shooter.setHoodAngle(Math.PI * 1);
+    // } else if (OI.driverController.getBButton()) {
+    //   shooter.setHoodAngle(Math.PI * 2);      
+    // } else if (OI.driverController.getXButton()) {
+    //   shooter.setHoodAngle(Math.PI * 3);
+    // } else if (OI.driverController.getYButton()) {
+    //   shooter.setHoodAngle(Math.PI * 4);
+    // } else {
+    //   shooter.setHoodAngle(0);
+    // }
+    // shooter.setHoodAngle(OI.driverController.getRawAxis(1)*Math.PI*5);
+
+    // System.out.println("HOOD: " + shooter.getHoodAngle() + ":" + shooter.getHoodVelocity());
+    // System.out.println("TGT: " + input);
+
+    // motor.set(0.25 * OI.driverController.getRawAxis(1));
   }
 
   // Returns true when the command should end.
