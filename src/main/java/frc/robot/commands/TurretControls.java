b55617ef798e19614005a7e544dae2d1892e73c1
Alex <alex.h@frc1073.org>
2020-02-23 15:15:37 -0500

    Housekeeping 2.0

--- src/main/java/frc/robot/commands/TurretControls.java
+++ src/main/java/frc/robot/commands/TurretControls.java
@@ -7,11 +7,9 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.OI;
-import frc.robot.Robot;
 import frc.robot.subsystems.interfaces.TurretInterface;
 
 public class TurretControls extends CommandBase {
