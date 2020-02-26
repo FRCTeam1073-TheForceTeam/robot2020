82d2e4dca140a4b094f12123f00bf697b6dbbd15
Celia <36650015+classygirl44@users.noreply.github.com>
2020-02-25 20:23:56 -0500

    Commented out sendable chooser in robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -48,7 +48,7 @@ public class Robot extends TimedRobot {
   public static Bling bling;
   public static BlingControls blingControls;
   public static CommandBase driveAuto;
-  public static SendableChooser<Command> chooser;
+  // public static SendableChooser<Command> chooser;
   
   // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
