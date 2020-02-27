d484da9e492a039dfd3d70239fecb14fcccd583c
Celia <36650015+classygirl44@users.noreply.github.com>
2020-02-26 20:01:34 -0500

    No longer goes on forever

--- src/main/java/frc/robot/autoCommands/BlingB.java
+++ src/main/java/frc/robot/autoCommands/BlingB.java
@@ -35,11 +35,11 @@ public class BlingB extends CommandBase {
   @Override
   public void execute() {
       bling.LEDRainbow();
-      //if (time > 600) {
-      //    done = true;
-      //} else {
-      //    done = false;
-      //}
+      if (time > 6000) {
+          done = true;
+      } else {
+          done = false;
+      }
       done = false;
       time = time + 1;
   }
