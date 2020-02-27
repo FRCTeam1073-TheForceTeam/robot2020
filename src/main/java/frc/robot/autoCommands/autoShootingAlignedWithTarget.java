6438cce474831152a97a5abef7488595bec09856
Christian <wagener_christian@yahoo.com>
2020-02-27 12:25:57 -0500

    Commiting since someone else will be working on this

---
+++ src/main/java/frc/robot/autoCommands/autoShootingAlignedWithTarget.java
@@ -0,0 +1,24 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.autoCommands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
+public class autoShootingAlignedWithTarget extends SequentialCommandGroup {
+
+  /**
+   * Creates a new autoShootingAlignedWithTarget.
+   */
+  public autoShootingAlignedWithTarget() {
+    super();
+    new autoShootingCommandGroup(0.0, 0.762, 0.0, (2 * Math.PI) / 3, 0.5);
+  }
+}
---
+++ src/main/java/frc/robot/autoCommands/autoShootingCommandGroup.java
@@ -0,0 +1,57 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.autoCommands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.interfaces.DrivetrainInterface;
+import frc.robot.subsystems.interfaces.ShooterInterface;
+import frc.robot.subsystems.interfaces.TurretInterface;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
+public class autoShootingCommandGroup extends SequentialCommandGroup {
+
+  DrivetrainInterface drivetrain;
+  ShooterInterface shooter;
+  TurretInterface turret;
+  double drivetrainRotation;
+  double drivetrainDistance;
+  double turretRotation;
+  double hoodRotation;
+  double flywheelVelocity;
+
+  /**
+   * Creates a new autoShootingCommandGroup.
+   */
+  public autoShootingCommandGroup(double drivetrainRotation, double drivetrainDistance, double turretRotation, double hoodRotation, double flywheelVelocity) {
+    super();
+
+    this.drivetrainRotation = drivetrainRotation;
+    this.drivetrainDistance = drivetrainDistance;
+    this.turretRotation = turretRotation;
+    this.hoodRotation = hoodRotation;
+    this.flywheelVelocity = flywheelVelocity;
+
+    addCommands(
+      new autoTurn(drivetrain, drivetrainRotation),
+
+      new autoDriveForward(drivetrain, drivetrainDistance)
+      .alongWith(
+        new autoTurnTurret(turret, turretRotation),
+        new autoSetHood(shooter, hoodRotation)
+      ),
+
+      new autoSetFlywheel(shooter, flywheelVelocity)
+    );
+  }
+
+  public static autoShootingCommandGroup autoShootingMidOfField(){
+    return new autoShootingCommandGroup(0.0, 0.0, 0.0, 0.0, 0.0);
+  }
+}
