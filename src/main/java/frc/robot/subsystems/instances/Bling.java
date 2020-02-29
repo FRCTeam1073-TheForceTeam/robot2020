2e213ff48eb5865ab096c6671f285629a8656e65
Alex <alex.h@frc1073.org>
2020-02-29 10:22:54 -0500

    Update Bling.java

--- src/main/java/frc/robot/subsystems/instances/Bling.java
+++ src/main/java/frc/robot/subsystems/instances/Bling.java
@@ -18,7 +18,6 @@ public class Bling extends SubsystemBase implements BlingInterface {
   private int counter_rainbow = 0;
   private int move_rainbow = 0;
 
-
   public Bling() {
     m_led = new AddressableLED(7);
     m_ledBuffer = new AddressableLEDBuffer(29);
@@ -41,7 +40,7 @@ public class Bling extends SubsystemBase implements BlingInterface {
     for (int i = min; i < (max); i = i + 2) {
       m_ledBuffer.setRGB(i, r1, g1, b1);
     }
-    
+
     for (int j = min + 1; j < (max); j = j + 2) {
       m_ledBuffer.setRGB(j, r2, g2, b2);
     }
@@ -64,14 +63,14 @@ public class Bling extends SubsystemBase implements BlingInterface {
     }
     m_led.setData(m_ledBuffer);
   }
-  
+
   // alternateHSV() has the same functionality as alternateRGB() except with HSV (hue, saturation + value)
   public void alternateHSV(int min, int number, int h1, int s1, int v1, int h2, int s2, int v2) {
     int max = min + number;
     for (int i = min; i < (max); i = i + 2) {
       m_ledBuffer.setHSV(i, h1, s1, v1);
     }
-    
+
     for (int j = min + 1; j < (max); j = j + 2) {
       m_ledBuffer.setHSV(j, h2, s2, v2);
     }
@@ -81,14 +80,14 @@ public class Bling extends SubsystemBase implements BlingInterface {
   // rangeHSV() same as rangeRGB() except using HSV values
   public void rangeHSV(int min, int number, int h, int s, int v) {
     int max = min + number;
-    
+
     for (int i = min; i < (max); i++) {
       m_ledBuffer.setHSV(i, h, s, v);
     }
 
     m_led.setData(m_ledBuffer);
   }
-  
+
   public void setLED(int i, int r, int g, int b) {
     m_ledBuffer.setRGB(i, r, g, b);
     m_led.setData(m_ledBuffer);
@@ -100,7 +99,7 @@ public class Bling extends SubsystemBase implements BlingInterface {
     m_ledBuffer.setRGB(i2, r, g, b);
     m_led.setData(m_ledBuffer);
   }
-  
+
   public void setLEDFromColor(int i) {
     Color blue = Color.kBlue;
     m_ledBuffer.setLED(i, blue);
@@ -112,38 +111,32 @@ public class Bling extends SubsystemBase implements BlingInterface {
 
   public void LEDRainbow() {
     int numLEDs = m_ledBuffer.getLength();
-    
+
     for (int i = 0; i < numLEDs; i++) {
-      
+
       if (((i + move_rainbow) % 12) == 0 || ((i + move_rainbow) % 12) == 1) {
         // Sets first LED, then sets every 6 after it "red"
         m_ledBuffer.setRGB(i, 255, 0, 0);
-      } 
-        else if (((i + 2 + move_rainbow) % 12) == 0 || ((i + 2 + move_rainbow) % 12) == 1) {
+      } else if (((i + 2 + move_rainbow) % 12) == 0 || ((i + 2 + move_rainbow) % 12) == 1) {
         // Sets second LED, then sets every 6 after it "orange"
         m_ledBuffer.setRGB(i, 255, 69, 0);
-      } 
-        else if (((i + 4 + move_rainbow) % 12) == 0 || ((i + 4 + move_rainbow) % 12) == 1) {
+      } else if (((i + 4 + move_rainbow) % 12) == 0 || ((i + 4 + move_rainbow) % 12) == 1) {
         // Sets third LED, then sets every 6 after it "yellow"
         m_ledBuffer.setRGB(i, 252, 227, 0);
-      } 
-        else if (((i + 6 + move_rainbow) % 12) == 0 || ((i + 6 + move_rainbow) % 12) == 1) {
+      } else if (((i + 6 + move_rainbow) % 12) == 0 || ((i + 6 + move_rainbow) % 12) == 1) {
         // Sets fourth LED, then sets every 6 after it "green"
         m_ledBuffer.setRGB(i, 0, 255, 0);
-      } 
-        else if (((i + 8 + move_rainbow) % 12) == 0 || ((i + 8 + move_rainbow) % 12) == 1) {
+      } else if (((i + 8 + move_rainbow) % 12) == 0 || ((i + 8 + move_rainbow) % 12) == 1) {
         // Sets fifth LED, then sets every 6 after it "blue"
         m_ledBuffer.setRGB(i, 0, 0, 255);
-      } 
-        else if (((i + 10 + move_rainbow) % 12) == 0 || ((i + 10 + move_rainbow) % 12) == 1) {
+      } else if (((i + 10 + move_rainbow) % 12) == 0 || ((i + 10 + move_rainbow) % 12) == 1) {
         // Sets sixth LED, then sets every 6 after it "purple"
         m_ledBuffer.setRGB(i, 128, 0, 128);
       }
-      
+
       if (counter_rainbow < 100) {
         counter_rainbow++;
-      } 
-        else {
+      } else {
         move_rainbow++;
         counter_rainbow = 1;
       }
