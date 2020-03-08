f9223382c16d7986ab84ab1cb4001bf3da9496f8
Celia <36650015+classygirl44@users.noreply.github.com>
2020-03-08 17:02:20 -0400

    Added back in execute in magazine

--- src/main/java/frc/robot/commands/MagazineControls.java
+++ src/main/java/frc/robot/commands/MagazineControls.java
@@ -22,7 +22,7 @@ public class MagazineControls extends CommandBase {
   int cellCount;
   public MagazineControls(MagazineInterface magazine_) {
     magazine = magazine_;
-    cellCount = 1;
+    cellCount = 0;
     addRequirements((SubsystemBase)magazine);
   }
 
@@ -35,19 +35,19 @@ public class MagazineControls extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    // if(OI.operatorController.getPOV() == 0){
-    //   magazine.setPower(.5);
-    // }
-    // else if(OI.operatorController.getPOV() == 180){
-    //   magazine.setPower(-.5);
-    // }
-    // else magazine.setPower(0);
+    if(OI.operatorController.getPOV() == 0){
+      magazine.setPower(.5);
+    }
+    else if(OI.operatorController.getPOV() == 180){
+      magazine.setPower(-.5);
+    }
+    else magazine.setPower(0);
 
-    // // magazine.updateCellCount();
-    // cellCount = magazine.getCellCount();
-    // SmartDashboard.putNumber("Cell Count: ", cellCount);
-    // SmartDashboard.putBoolean("Enterance: ", magazine.getEnteranceState());
-    // SmartDashboard.putBoolean("Exit: ", magazine.getExitState());
+    // magazine.updateCellCount();
+    cellCount = magazine.getCellCount();
+    SmartDashboard.putNumber("Cell Count: ", cellCount);
+    SmartDashboard.putBoolean("Enterance: ", magazine.getEnteranceState());
+    SmartDashboard.putBoolean("Exit: ", magazine.getExitState());
   }
 
   // Returns true when the command should end.
